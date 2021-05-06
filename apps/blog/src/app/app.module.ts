b54ce029b5f1e19a13d4c03cf703bddc1ba31e27
Sean Collins <sean@uncannycode.com>
2021-05-05 23:02:42 -0400

    init

--- apps/blog/src/app/app.module.ts
+++ apps/blog/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [ BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
